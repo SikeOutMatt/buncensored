c5b8c701fd0bb61353a3305968ccc2e343a3857b
Matthew Perrais <matthewperrais@Matthews-MacBook-Pro.local>
2023-11-28 19:34:06 -0800

    Added new homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,60 @@
 <script>
     import background from '../lib/assets/background.webp'
     import videobackground from '../lib/assets/videobackground.mp4'
+    import banner from '../lib/assets/frontpage-banner-2.webp';
+    import Logo from '../lib/assets/logo.webp';
 
 </script>
 
 
 
 <main> 
-    <div class="videoContainer"> 
-        <video autoplay>
-            <source src={videobackground} type="video/mp4" />
-        </video>
-    </div>
-    <div class=" buttonContainer"> 
-        <button class=""> JOIN NOW</button>
-        <a href="/account"><button> LOGIN</button> </a>
+    <div class="container"> 
+        <div class="grid background"> 
+            <div>
+                <div>
+                    <img class="hidden" src={banner}>
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <div class="flex background">
+                <div class="loginOptions">
+                    <img src={Logo} width="350px" height="200px">
+                    <button> Sign Up </button>
+                    <button> Login </button>
+                    <h1> Sign In With Your Favorite Apps</h1>
+                    <button> Sign in With Twitter </button>
+                    <button> Sign in With Facebook </button>
+                    <button> Sign in With Google </button>
+                </div>
+    
+            </div>
+        </div>
+
+
+
+
     </div>
 </main>
 
-
 <style>
 
+    main {
+        margin: -10px;
 
+    }
+
+h1 {
+    color: white;
+    font-size: 15px;
+    font-weight: bolder;
+}
+
+.hidden {
+    overflow: hidden;
+}
 
 .container {
     width: 100vw;
@@ -31,69 +64,71 @@
     
 }
 
-.relative {
-    position: relative;
-}
-
-.absolute {
-    position: absolute;
-}
-
 button {
     background-color: #00AEEF;;
     color: #fff;
     border: none;
-    padding: 20px 30px;
-    margin: 10px;
+    padding: 10px 20px;
     border-radius: 5px;
-    font-size: 1.2rem;
+    font-size: 15px;
     cursor: pointer;
-    position: flex;
+    margin-right: 20px;
+
+}
+
+
+.background {
+    background-color: #2a2a2a;
+    margin: 0px;
+}
+
+
+.grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+img {
+    overflow: hidden;
+    margin: 0;
+}
+   
+.flex {
+    display: flex;
     justify-content: center;
     align-items: center;
-    transform: translate(50%,50%);
+}
+
+input {
+    margin: 20px;
+    border-radius: 5px;
+    padding: 5px;
+    border-style: none;
+    font-size: 15px;
+    background-color: #6a6a6a;
+}
+
+.loginOptions button {
+    margin: 20px;
+    border-radius: 20px;
+    padding: 5px;
+    border-style: none;
+    font-size: 15px;
+    background-color: #00AEEF;
+    width: 200%;
+    padding: 10px 20px;
 
 }
 
-.buttonContainer {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    width: 100%;
-    height: 100%;
+.loginOptions {
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+
 }
 
-.buttonContainer button {
-    position: relative;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-}
-
-.videoContainer {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%; 
-  overflow: hidden;
-}
-.videoContainer video {
-  /* Make video to at least 100% wide and tall */
-  min-width: 100%; 
-  min-height: 100%; 
-
-  /* Setting width & height to auto prevents the browser from stretching or squishing the video */
-  width: auto;
-  height: auto;
-
-  /* Center the video */
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%,-50%);
-}
+    
 
 </style>
 
